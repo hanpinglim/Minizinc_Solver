754b53f2762025425c9556dd5d7840bb1b37728c
Yepu Hou <yhou0023@student.monash.edu>
2024-03-07 11:51:45 +1100

    minizinc problem

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import minizinc
 
 # Load the Sudoku model
-model = minizinc.Model("./sudoku_model.mzn")
+model = minizinc.Model("./mznc2023_probs/sudoku_fixed/sudoku_fixed.mzn")
 
 # Create a MiniZinc solver instance
 gecode_solver = minizinc.Solver.lookup("gecode")
@@ -10,12 +10,23 @@ gecode_solver = minizinc.Solver.lookup("gecode")
 instance = minizinc.Instance(gecode_solver, model)
 instance.add_file("./sudoku_p0.dzn")
 
-# Solve the instance
+# Set the initial values of the Sudoku grid
+instance["grid"] = [
+    [0, 0, 0, 2, 0, 5, 0, 0, 0],
+    [0, 9, 0, 0, 0, 0, 7, 3, 0],
+    [0, 0, 2, 0, 0, 9, 0, 6, 0],
+    [2, 0, 0, 0, 0, 0, 4, 0, 9],
+    [0, 0, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 9, 0, 0, 0, 0, 0, 1],
+    [0, 8, 0, 4, 0, 0, 1, 0, 0],
+    [0, 6, 3, 0, 0, 0, 0, 8, 0],
+    [0, 0, 0, 6, 0, 8, 0, 0, 0]
+]
+# Set the value of n
+instance["n"] = 9
+# Solve the problem
 result = instance.solve()
 
-# Check and print the result
-if result.status == minizinc.result.Status.SATISFIED:
-    print("Solution found:")
-    print(result.solution)
-else:
-    print("No solution found:", result.status)
+# Print the solution
+print(result)
+

a74b71fbaacb9c0b2fb9feb845fa8bad906388af
Han Ping Lim <hlim0036@student.monash.edu>
2024-03-07 12:39:55 +1100

    path change

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import minizinc
 
 # Load the Sudoku model
-model = minizinc.Model("./mznc2023_probs/sudoku_fixed/sudoku_fixed.mzn")
+model = minizinc.Model("./Minizinc_Solver/mznc2023_probs/sudoku_fixed/sudoku_fixed.mzn") #Minizinc_Solver\mznc2023_probs\sudoku_fixed\sudoku_fixed.mzn
 
 # Create a MiniZinc solver instance
 gecode_solver = minizinc.Solver.lookup("gecode")
 
 # Create an instance of the model and link the data file
 instance = minizinc.Instance(gecode_solver, model)
-instance.add_file("./mznc2023_probs/sudoku_fixed/sudoku_p0.dzn")
+instance.add_file("./Minizinc_Solver/mznc2023_probs/sudoku_fixed/sudoku_p20.dzn")
 
 # Solve the problem
 result = instance.solve()

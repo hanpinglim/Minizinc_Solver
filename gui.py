1e7ec843fb0b13f94946781d137a290cc053edf5
houyepu <yhou0023@student.monash.edu>
2024-03-21 18:37:00 +1100

    gui generate instances are working for kindey exchange problem

--- gui.py
+++ gui.py
@@ -1,9 +1,40 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
+import random
+
+def generate_instance(V, K):
+    edge_weight = '[|'
+    for i in range(V):
+        for j in range(V):
+            if i == j:
+                edge_weight += '0,'
+            else:
+                weight = random.randint(-100, 100)
+                edge_weight += str(weight) + ','
+        edge_weight += '|'
+    edge_weight += ']'
+
+    instance = f"""
+K = {K};
+V = {V};
+edge_weight = {edge_weight};
+"""
+    return instance
+
+def save_instances(num_instances, V, K, filename):
+    for i in range(num_instances):
+        with open('instances/'+str(i)+filename, "w") as f:
+            instance = generate_instance(V, K)
+            f.write(instance)
+            f.write("\n")
 
 def generate_instances():
-    # Your code to generate instances
-    messagebox.showinfo("Generate Instances", "Instances generated successfully!")
+    num_instances = 1000
+    V = 25
+    K = 3
+    filename = "instance.dzn"
+    save_instances(num_instances, V, K, filename)
+    messagebox.showinfo("Generate Instances", f"Generated {num_instances} instances and saved to {filename}")
 
 def solve_instances():
     # Your code to solve instances
